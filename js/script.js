a1fc596bd046eef22914d84b1a3823dc7ae5617b
Daveedick <davvo77@gmail.com>
2023-06-08 15:23:36 +0530

    Create another version fo chinese vresion pdf

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-	const langSwitcher = document.querySelectorAll('.js-btn[data-purpose="langSwitcher"]');
+// 	const langSwitcher = document.querySelectorAll('.js-btn[data-purpose="langSwitcher"]');
 
-	langSwitcher.forEach(btn => {
+// 	langSwitcher.forEach(btn => {
 
-		const langs = {
-			ru: 'ru',
-			en: 'en'
-		}
-		const views = {
-			ru: document.querySelector('#ruV'),
-			en: document.querySelector('#enV')
-		}
+// 		const langs = {
+// 			ru: 'ru',
+// 			en: 'en'
+// 		}
+// 		const views = {
+// 			ru: document.querySelector('#ruV'),
+// 			en: document.querySelector('#enV')
+// 		}
 
-		function setlang(l) {
-			document.documentElement.setAttribute('lang', l)
-			docLang = l;
-		}
+// 		function setlang(l) {
+// 			document.documentElement.setAttribute('lang', l)
+// 			docLang = l;
+// 		}
 
-		function switchView() {
-			const docLang = document.documentElement.getAttribute('lang');
-			if (docLang === langs.en) {
-				setlang(langs.ru)
-				views.en.style.display = 'none';
-				views.ru.style.display = 'block';
-			} else if (docLang === langs.ru) {
-				setlang(langs.en)
-				views.ru.style.display = 'none';
-				views.en.style.display = 'block';
-			}
-		}
+// 		function switchView() {
+// 			const docLang = document.documentElement.getAttribute('lang');
+// 			if (docLang === langs.en) {
+// 				setlang(langs.ru)
+// 				views.en.style.display = 'none';
+// 				views.ru.style.display = 'block';
+// 			} else if (docLang === langs.ru) {
+// 				setlang(langs.en)
+// 				views.ru.style.display = 'none';
+// 				views.en.style.display = 'block';
+// 			}
+// 		}
 
-		btn.addEventListener('click', (e) => {
-			e.preventDefault();
-			switchView();
-		})
-	})
-})
+// 		btn.addEventListener('click', (e) => {
+// 			e.preventDefault();
+// 			switchView();
+// 		})
+// 	})
+// })
